bba257b1a42d9f8a63fedf82f2007b9665c2577c
Leandro Lima <60799765+leandrolls@users.noreply.github.com>
2022-03-11 19:56:11 -0300

    Update credentials.py

--- crendentials/credentials.py
+++ crendentials/credentials.py
@@ -2,7 +2,7 @@ import starkbank
 import os
 
 #In webhook_url put url of server.
-webhook_url = 'https://webhook.site/302404de-7031-4ff6-89e5-6f9952ee59c7'
+webhook_url = ''
 
 def credentials_init():
 
